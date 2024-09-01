04b124fa7fcba183d1f6ee9f90f89e023d641d34
Gabriel Santos <172639817+gs-101@users.noreply.github.com>
2024-09-01 01:32:13 +0000

    feat: init.el
    Initialization configuration file.

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+
+(add-to-list 'load-path (expand-file-name modules-directory))
+
+(require 'gs-package)
+(require 'gs-settings)
+(require 'gs-fm)
+(require 'gs-keys)
+(require 'gs-org)
+(require 'gs-org-roam)
+(require 'gs-cmd)
+(require 'gs-dev)
+(require 'gs-completion)
+(require 'gs-utils)
+(require 'gs-keys-evil)
+(require 'gs-ui)
