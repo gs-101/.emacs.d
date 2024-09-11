977d71829fa21b8b1187cee9d181cef32a8c80d9
Gabriel <172639817+gs-101@users.noreply.github.com>
2024-09-11 08:49:00 -0300

    feat: separedit
    Pops comments to a separate buffer, like `org-edit-special`

--- modules/gs-settings.el
+++ modules/gs-settings.el
@@ -219,6 +219,14 @@
   (custom-file (no-littering-expand-etc-file-name "custom.el"))
   )
 
+(use-package separedit
+  :bind
+  ("C-c C-." . separedit)
+  :custom
+  (separedit-default-mode 'org-mode)
+  :ensure t
+  )
+
 (provide 'gs-settings)
 
 (use-package autorevert
