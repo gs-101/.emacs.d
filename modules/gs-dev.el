42c8a0d10ec297981493f5464ceeed359355b8bf
Gabriel <172639817+gs-101@users.noreply.github.com>
2024-09-13 14:13:03 -0300

    feat: defer leetcode
    Defer leetcode package loading by binding it to its own commands

--- modules/gs-dev.el
+++ modules/gs-dev.el
@@ -152,6 +152,11 @@
   )
 
 (use-package leetcode
+  :commands
+  (
+   leetcode
+   leetcode-daily
+   )
   :custom
   (leetcode--paid "$")
   (leetcode--User-Agent ("User Agent" . "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.1"))
