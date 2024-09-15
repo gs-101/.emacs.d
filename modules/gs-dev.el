42dbcbef845a9fd9de7b337490d457572b79441a
Gabriel <172639817+gs-101@users.noreply.github.com>
2024-09-15 06:40:38 -0300

    feat: gptel
    A simple LLM client for Emacs

--- modules/gs-dev.el
+++ modules/gs-dev.el
@@ -187,7 +187,41 @@
    )
   )
 
+(use-package gptel
+  :commands
+  (
+   gptel
+   gptel-menu
+   )
+  :custom
+  (gptel-default-mode 'org-mode)
+  :ensure t
+  )
+
+(use-package gptel-openai
+  :config
+  (gptel-make-openai "Groq"
+    :host "api.groq.com"
+    :endpoint "/openai/v1/chat/completions"
+    :stream nil
+    :key #'gptel-api-key
+    :models '(
+              "llama-3.1-70b-versatile"
+              "llama-3.1-8b-instant"
+              "llama3-70b-8192"
+              "llama3-8b-8192"
+              "mixtral-8x7b-32768"
+              "gemma-7b-it"
+              ))
+  )
+
 (use-package grip-mode
+  :commands
+  (
+   grip-start-preview
+   )
+  :custom
+  (grip-preview-use-webkit nil)
   :ensure t
   )
 
