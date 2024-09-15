ba27829108efc6248fb7e34d3dfbeb0670bbb57d
Gabriel <172639817+gs-101@users.noreply.github.com>
2024-09-15 06:36:22 -0300

    feat: vterm `project-shell`
    Use vterm as the project's shell

--- modules/gs-cmd.el
+++ modules/gs-cmd.el
@@ -7,6 +7,28 @@
   :ensure t
   )
 
+(use-package vterm
+  :preface
+  (defun mocompute/project-shell ()
+    "Start an inferior shell in the current project's root directory.
+If a buffer already exists for running a shell in the project's root,
+switch to it.  Otherwise, create a new shell buffer.
+With \\[universal-argument] prefix arg, create a new inferior shell buffer even
+if one already exists."
+    (interactive)
+    (require 'comint)
+    (let* ((default-directory (project-root (project-current t)))
+           (default-project-shell-name (project-prefixed-buffer-name "shell"))
+           (shell-buffer (get-buffer default-project-shell-name)))
+      (if (and shell-buffer (not current-prefix-arg))
+          (if (comint-check-proc shell-buffer)
+              (pop-to-buffer shell-buffer (bound-and-true-p display-comint-buffer-action))
+            (vterm shell-buffer))
+        (vterm (generate-new-buffer-name default-project-shell-name)))))
+  :config
+  (advice-add 'project-shell :override #'mocompute/project-shell)
+  )
+
 (use-package vterm-toggle
   :bind
   ("C-c t v" . vterm-toggle)
