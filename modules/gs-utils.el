2f58b69fcbf16d2d5eac60be608a5297afbcc370
gs-101 <172639817+gs-101@users.noreply.github.com>
2024-12-05 14:02:13 -0300

    feat: geiser
    Improved Scheme REPL.

--- modules/gs-utils.el
+++ modules/gs-utils.el
@@ -373,6 +373,7 @@ targets."
   (popper-group-function 'popper-group-by-project)
   (popper-reference-buffers '(
                               compilation-mode
+                              geiser-repl-mode
                               vterm-mode
                               inferior-emacs-lisp-mode
                               inferior-lisp-mode
