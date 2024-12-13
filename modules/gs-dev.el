61c6741cd5501167f4744b88818213a36037d527
gs-101 <172639817+gs-101@users.noreply.github.com>
2024-12-13 09:47:02 -0300

    feat: python-pytest.el
    Run pytest inside Emacs.

--- modules/gs-dev.el
+++ modules/gs-dev.el
@@ -269,6 +269,15 @@
                          python-shell-virtualenv-root (pet-virtualenv-root))))
   )
 
+(use-package python-pytest
+  :bind
+  (
+   :map python-base-mode-map
+   ("C-c m t" . python-pytest-dispatch)
+   )
+  :ensure t
+  )
+
 (use-package cargo-transient
   :bind
   (
