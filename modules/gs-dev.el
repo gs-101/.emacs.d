7c7e373cac140b9a76e037be1ed52a41b32b55f0
gs-101 <172639817+gs-101@users.noreply.github.com>
2024-12-19 08:34:35 -0300

    feat: copilot
    GitHub Copilot is free now!

--- modules/gs-dev.el
+++ modules/gs-dev.el
@@ -378,6 +378,25 @@ If it is, enable `aggressive-indent-mode'."
   (prog-mode . apheleia-mode)
   )
 
+(use-package copilot
+  :ensure t
+  :bind
+  (
+   :map copilot-completion-map
+   ("<tab>" . copilot-accept-completion)
+   ("TAB" . copilot-accept-completion)
+   ("C-<tab>" . copilot-accept-completion-by-word)
+   ("C-TAB" . copilot-accept-completion-by-word)
+   ("C-n" . copilot-next-completion)
+   ("C-p" . copilot-previous-completion)
+   )
+  :config
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode 2))
+  :hook
+  (prog-mode . copilot-turn-on-unless-buffer-read-only)
+  )
+
 (use-package combobulate
   :vc (:url "https://github.com/mickeynp/combobulate")
   :bind
