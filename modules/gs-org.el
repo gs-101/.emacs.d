ba6b9b7a913045f6076671a6f53970f85badcbf0
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-01-07 09:21:36 -0300

    feat: `org-latex-pdf-process`
    Add the `bibtex` command to `org-latex-pdf-process` to process bibliographies.

--- modules/gs-org.el
+++ modules/gs-org.el
@@ -219,6 +219,7 @@ allowFullScreen>
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (add-to-list 'org-latex-pdf-process "bibtex %b" t)
   :defer t
   )
 
