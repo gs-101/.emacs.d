e7b40cf98a196cf45a9aa12ac1a10253a2a39f8a
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-27 22:16:43 -0300

    revert: usage of ferris for match arms
    Remove the replacement of match arms with Ferris. I like Ferris, but
    this change doesn't fit as much as the Go gopher does.

--- modules/gs-ui.el
+++ modules/gs-ui.el
@@ -483,13 +483,6 @@ instead of $."
                   (push '(":=" . ?) prettify-symbols-alist)
                   (push '("go" . ?󰟓) prettify-symbols-alist))))
 
-(use-package rust-ts-mode
-  :after nerd-icons rust-ts-mode
-  :hook
-  (rust-ts-mode . prettify-symbols-mode)
-  (rust-ts-mode . (lambda ()
-                    (push '("=>" . ?) prettify-symbols-alist))))
-
 (use-package nerd-icons-ibuffer
   :vc (:url "https://github.com/seagle0128/nerd-icons-ibuffer")
   :after nerd-icons
