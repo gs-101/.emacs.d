9f06eeed4ea4bc99d5495411038637eed690c88e
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-04-15 08:15:37 -0300

    feat: copilot.el

--- modules/gs-llm.el
+++ modules/gs-llm.el
@@ -14,4 +14,18 @@
   :vc (:url "https://github.com/karthink/gptel-agent")
   :ensure t)
 
+(use-package copilot
+  :vc (:url "https://github.com/copilot-emacs/copilot.el")
+  :ensure t
+  :bind
+  (:map copilot-completion-map
+        ("<tab>" . copilot-accept-completion)
+        ("TAB" . copilot-accept-completion)
+        ("C-<tab>" . copilot-accept-completion-by-word)
+        ("C-TAB" . copilot-accept-completion-by-word)
+        ("C-n" . copilot-next-completion)
+        ("C-p" . copilot-previous-completion))
+  :custom
+  (copilot-indent-offset-warning-disable t))
+
 (provide 'gs-llm)
