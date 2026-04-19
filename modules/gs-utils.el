71c2fd71e56399be834c9771510b15befe425195
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-04-19 10:33:54 -0300

    revert: pass
    I replaced password-store with gopass now.

--- modules/gs-utils.el
+++ modules/gs-utils.el
@@ -272,11 +272,6 @@ using Helpful."
   ("M-s p" . p-search)
   :ensure t)
 
-(use-package pass
-  ;; Commit is pinned for security reasons.
-  :vc (:url "https://github.com/NicolasPetton/pass" :rev "143456809fd2dbece9f241f4361085e1de0b0e75")
-  :ensure t)
-
 (use-package pinentry
   :init
   (pinentry-start)
