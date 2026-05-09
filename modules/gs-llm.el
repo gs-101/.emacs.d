bdc93a5949b4fca47b2b06a38d38aaa06e246b4e
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-05-09 11:53:24 -0300

    feat: eca

--- modules/gs-llm.el
+++ modules/gs-llm.el
@@ -1,31 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
-(use-package gptel
-  :vc (:url "https://github.com/karthink/gptel")
-  :ensure-system-package
-  curl
-  :ensure t
-  :config
-  (setq gptel-model 'claude-opus-4.6
-        gptel-backend
-        (gptel-make-gh-copilot "Copilot")))
-
-(use-package gptel-agent
-  :vc (:url "https://github.com/karthink/gptel-agent")
+(use-package eca
+  :vc (:url "https://github.com/editor-code-assistant/eca-emacs")
   :ensure t)
-
-(use-package copilot
-  :vc (:url "https://github.com/copilot-emacs/copilot.el")
-  :ensure t
-  :bind
-  (:map copilot-completion-map
-        ("<tab>" . copilot-accept-completion)
-        ("TAB" . copilot-accept-completion)
-        ("C-<tab>" . copilot-accept-completion-by-word)
-        ("C-TAB" . copilot-accept-completion-by-word)
-        ("C-n" . copilot-next-completion)
-        ("C-p" . copilot-previous-completion))
-  :custom
-  (copilot-indent-offset-warning-disable t))
 
 (provide 'gs-llm)
